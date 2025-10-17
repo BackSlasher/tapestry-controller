fd186d9fca20b90abc384636ecbb153bea461ed2
Nitzan Raz <nitz.raz@gmail.com>
2025-10-18 00:40:27 +0300

    connector confusion

--- src/tapestry/screen_types.py
+++ src/tapestry/screen_types.py
@@ -15,9 +15,9 @@ class ScreenType(NamedTuple):
 SCREEN_TYPES = {
     # https://www.panelook.com/ED060XC3_E_Ink_6.0_EPD_parameter_21976.html
     "ED060XC3": ScreenType(
-        name="ED060XC3", description='6.0" E-Paper Display', connector="33"
+        name="ED060XC3", description='6.0" E-Paper Display', connector="34"
     ),
     "ED097TC2": ScreenType(
-        name="ED097TC2", description='9.7" E-Paper Display', connector="34"
+        name="ED097TC2", description='9.7" E-Paper Display', connector="33"
     ),
 }
